e07e10384ff3138fa3403a366bc959831d7a9248
indu <induls1992@gmail.com>
2021-08-28 20:46:53 +0530

    Changes in server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get('/', (req, res)=>{
 if(process.env.NODE_ENV){
     app.use('/', express.static('client/build'));
     app.get("*", (req, res) => {
-        res.sendFile(path.reolve(__dirname, 'client/build/index.html'))
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
     })
 }
 
