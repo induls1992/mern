71ca9063520742575748fe867282df286d5c8b67
indu <induls1992@gmail.com>
2021-08-28 21:05:19 +0530

    Changes in serverjs file

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.get('/', (req, res)=>{
 });
 
 if(process.env.NODE_ENV){
-    app.use('/', express.static(path.join(__dirname, "client/build")));
+    app.use('/', express.static("client/build"));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+        res.sendFile(path.resolve(__dirname, "client/build/index.html"))
     })
 }
 
 const port = process.env.PORT || 5000;
 
-app.listen(5000, ()=>console.log(`Server is listening on port ${port}`));
+app.listen(port, ()=>console.log(`Server is listening on port ${port}`));
