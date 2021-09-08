4b8aa586ad15d317570bcf501a105fd7a5a9fdb8
indu <induls1992@gmail.com>
2021-09-09 00:58:31 +0530

    App.test.js unit testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import HomeScreen from './screens/HomeScreen';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import { Route } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
+import ProductDescription from './screens/ProductDescription';
+Enzyme.configure({ adapter: new Adapter() });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+let pathMap = {};
+describe('routes using array of routers', () => {
+  beforeAll(() => {
+    const component = shallow(<App/>);
+    pathMap = component.find(Route).reduce((pathMap, route) => {
+        const routeProps = route.props();
+        pathMap[routeProps.path] = routeProps.component;
+        return pathMap;
+      }, {});
+  })
+  it('should show Homescreen component for / router (getting array of routes)', () => {
+    expect(pathMap['/']).toBe(HomeScreen);
+  })
+  it('should show Cart component for /cart router', () => {
+    expect(pathMap['/cart']).toBe(CartScreen);
+  })
+  // it('should show Product Description /product router', () => {
+  //   expect(pathMap['/product']).toBe(ProductDescription);
+  // })
+})
